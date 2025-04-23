80897bc8747c22bffa964005a0cf844c99caba6b
UCAS-John <john.wangwang@ucas-edu.net>
2025-04-23 13:39:22 -0600

    png

--- games/cookie_clicker/main.py
+++ games/cookie_clicker/main.py
@@ -1,3 +1,7 @@
+import os
+
+cookie_pic = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "..", "images", "cookie.png")
+
 def main():
     raise NotImplementedError("This game is not implemented yet.")
 
