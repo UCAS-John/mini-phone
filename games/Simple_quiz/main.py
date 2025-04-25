f4e47ad1ee07495c16af1f920852289ea3698fd8
Gavinatoraider <gavin.pierce@ucas-edu.net>
2025-04-25 13:43:21 -0600

    started quiz

--- games/Simple_quiz/main.py
+++ games/Simple_quiz/main.py
@@ -1,13 +1,41 @@
+import os
+
 def main():
     print("""
-     _____ _                 __        ____        _           ______        __     _   __      __  _ 
-    / ___/(_)___ ___  ____  / /__     / __ \__  __(_)___     _/_/ __ )__  __/ /_   / | / /___  / /_| |
-    \__ \/ / __ `__ \/ __ \/ / _ \   / / / / / / / /_  /    / // __  / / / / __/  /  |/ / __ \/ __// /
-    ___/ / / / / / / / /_/ / /  __/  / /_/ / /_/ / / / /_   / // /_/ / /_/ / /_   / /|  / /_/ / /_ / / 
-    /____/_/_/ /_/ /_/ .___/_/\___/   \___\_\__,_/_/ /___/  / //_____/\__,_/\__/  /_/ |_/\____/\__//_/  
-                    /_/                                     |_|                                  /_/    
-""")
+  _________.__               .__           ________        .__             ___ __________        __     _______          __    ___    
+ /   _____/|__| _____ ______ |  |   ____   \_____  \  __ __|__|_______    /  / \______   \__ ___/  |_   \      \   _____/  |_  \  \   
+ \_____  \ |  |/     \,\____ \|  | _/ __ \   /  / \  \|  |  \  \___   /   /  /   |    |  _/  |  \   __\  /   |   \ /  _ \   __\  \  \  
+ /        \|  |  Y Y  \  |_> >  |_\  ___/  /   \_/.  \  |  /  |/    /   (  (    |    |   \  |  /|  |   /    |    (  <_> )  |     )  ) 
+/_______  /|__|__|_|  /   __/|____/\___  > \_____\ \_/____/|__/_____ \   \  \   |______  /____/ |__|   \____|__  /\____/|__|    /  /  
+        \/          \/|__|             \/         \__>              \/    \__\         \/                      \/              /__/   """)
 
+difficulty=input("what is the difficulty you would like for you quiz? (Easy = 1 , medium = 2, hard = 3, impossible = 4) ")
+
+while True:
+    if difficulty == "1":
+        start = input(" you chose the easy level, this quiz is close note, internet and friend. press space and then enter when you are ready.")
+        if start == " ":
+            os.system('cls')
+
+            #quiz part will go here
+
+        else: print("brotha it ain't that hard to press space and enter. do it again!! ")
+
+    elif start == "2":
+        start = input(" you chose the easy level, this quiz is close note, internet and friend. press space and then enter when you are ready.")
+        if start == " ":
+            os.system('cls')
+        else: print("brotha it ain't that hard to press space and enter. do it again!! ")
+
+        #quiz part will go here
+
+    elif start == "2":
+        start = input(" you chose the easy level, this quiz is close note, internet and friend. press space and then enter when you are ready.")
+        if start == " ":
+            os.system('cls')
+        else: print("brotha it ain't that hard to press space and enter. do it again!! ")
+
+        #quiz part will go here
 
 if __name__ == "__main__":
     main()
