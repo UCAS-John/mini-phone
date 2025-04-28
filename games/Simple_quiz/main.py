0f731ee799c3b0f254688485111d70f413ca6b7e
Gavinatoraider <gavin.pierce@ucas-edu.net>
2025-04-28 13:35:37 -0600

    I did something

--- games/Simple_quiz/main.py
+++ games/Simple_quiz/main.py
@@ -2,12 +2,12 @@ import os
 
 def main():
     print("""
-  _________.__               .__           ________        .__             ___ __________        __     _______          __    ___    
- /   _____/|__| _____ ______ |  |   ____   \_____  \  __ __|__|_______    /  / \______   \__ ___/  |_   \      \   _____/  |_  \  \   
+  _________.__               . __           ________        .__             ___ __________        __     _______          __    ___    
+ /   _____/|__| _____ ,______ |  |   ____   \_____  \  __ __|__|_______    /  / \______   \__ ___/  |_   \      \   _____/  |_  \  \   
  \_____  \ |  |/     \,\____ \|  | _/ __ \   /  / \  \|  |  \  \___   /   /  /   |    |  _/  |  \   __\  /   |   \ /  _ \   __\  \  \  
- /        \|  |  Y Y  \  |_> >  |_\  ___/  /   \_/.  \  |  /  |/    /   (  (    |    |   \  |  /|  |   /    |    (  <_> )  |     )  ) 
-/_______  /|__|__|_|  /   __/|____/\___  > \_____\ \_/____/|__/_____ \   \  \   |______  /____/ |__|   \____|__  /\____/|__|    /  /  
-        \/          \/|__|             \/         \__>              \/    \__\         \/                      \/              /__/   """)
+ /        \|  |  Y Y  \|  |_> >  |_\  ___/  /   \_/.  \  |  /  |/    /   (  (    |    |   \  |  /|  |   /    |    (  <_> )  |     )  ) 
+/_______  /|__|__|_|  /|   __/|____/\___  > \_____\ \_/____/|__/_____ \   \  \   |______  /____/ |__|   \____|__  /\____/|__|    /  /  
+        \/          \/,|__|             \/         \__>              \/    \__\         \/                      \/              /__/   """)
 
 difficulty=input("what is the difficulty you would like for you quiz? (Easy = 1 , medium = 2, hard = 3, impossible = 4) ")
 
@@ -29,13 +29,16 @@ while True:
 
         #quiz part will go here
 
-    elif start == "2":
+    elif start == "3":
         start = input(" you chose the easy level, this quiz is close note, internet and friend. press space and then enter when you are ready.")
         if start == " ":
             os.system('cls')
         else: print("brotha it ain't that hard to press space and enter. do it again!! ")
 
         #quiz part will go here
+    
+    else:
+        break
 
 if __name__ == "__main__":
     main()
