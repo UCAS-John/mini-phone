554a49093e0ee2ef37848df5db6f40257372db5d
UCAS-John <john.wangwang@ucas-edu.net>
2025-04-28 13:34:15 -0600

    loading score

--- src/manage/scores.py
+++ src/manage/scores.py
@@ -18,12 +18,19 @@ def save_score(username: str, game: _TYPES, score: int):
 
     save_csv(PATH, data)
 
-def load_score(game: _TYPES):
+def load_score(username, game: _TYPES):
     data = read_csv(PATH)
     if data is None:
         return None
     if game not in data.columns:
         data = pd.DataFrame(columns=["username", "hangman", "number guessing", "rock paper scissors", "simon", "simple quiz", "tic tace toe"])
 
+    data = data[data["username"] == username]
+
+    if data.empty:
+        return None
+    return data.iloc[0][game]
+
 if __name__ == "__main__":
     save_score("test_user", "hangman", 100)
+    print(load_score("test_user", "hangman"))
