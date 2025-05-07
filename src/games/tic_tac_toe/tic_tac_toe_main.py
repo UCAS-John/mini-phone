aef3c1173c9fb02551bc5b710dbd4e25ae727265
AlishyaX <alishya.xavier@ucas-edu.net>
2025-05-07 13:43:55 -0600

    worked on score issues

--- src/games/tic_tac_toe/tic_tac_toe_main.py
+++ src/games/tic_tac_toe/tic_tac_toe_main.py
@@ -82,12 +82,5 @@ ___    __           ___       __           ___  __   ___
         #question = input('Do you want to keep playing ')
 
 #Lastly this uses a specific name the computer knows to start the program at the play_game function
-<<<<<<< HEAD:games/Tic-Tac-Toe/main.py
 if __name__ == "__main__":
    play_game()
-
-=======
-
-if __name__ == "__main__":
-    play_game()
->>>>>>> 9c14335b60e29af005f7f2194092965bed261270:games/Tic-Tac-Toe/tic_tac_toe_main.py
