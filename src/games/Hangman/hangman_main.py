06209359a08972991dea92d4c53d825d874a160d
scarggy-design1 <lizzy.saldana@ucas-edu.net>
2025-05-13 13:24:55 -0600

    added coments

--- src/games/Hangman/hangman_main.py
+++ src/games/Hangman/hangman_main.py
@@ -2,14 +2,14 @@ from faker import Faker
 
 fake = Faker()
 
-def word(score):
+def word(score): #makes a random word and creates the crypted version to display amount of letters
     rand_word = fake.word().lower()
     wordy = list(rand_word)
     letters = ['_|' for _ in wordy]
     score = guesser(letters, wordy, score)
     return score
 
-def guesser(letters, wordy, score):
+def guesser(letters, wordy, score): #This function is the actual game where the user guesses characters
     guessed_letters = set()
     wrong_guesses = 0
     max_wrong = 6
@@ -49,12 +49,10 @@ def guesser(letters, wordy, score):
             word(score)
         elif play == 'no' or play == 'n':
             print("Thanks for playing! ")
-            #Call the save score function and save the score
-            return 
+            return score
         else:
             print("PICK A VALID INPUT. Exiting game...")
-            #Call the save score function and save the score
-            return 
+            return score
             
 
 
@@ -65,14 +63,13 @@ def guesser(letters, wordy, score):
             print(ch, end='')
         play= input("\nDo you want to play again (y/n): ").lower()
         if play == 'yes' or play == 'y':
-            word()
+            word(score)
         elif play == 'no' or play == 'n':
             print("Thanks for playing! ")
-            #Call the save score function and save the score
-            return
+            return score
 
-def main():
-    print("insert the hangman font thingy")
+def main(): #Main user interface
+    print("-----HANGMAN-----\n")
     while True:
         options = input("what would you like to do?\n1) play\n2) exit\n")
         if options == '1':
@@ -82,7 +79,6 @@ def main():
             break
         else:
             print("Pick a valid choice.")
-        
     return score
 
 if __name__ == "__main__":
